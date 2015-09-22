88334651e505553fb6d7f147f72eef3c13a3289b
Aleksey Kurkov <aleksey.kurkov@gmail.com>
2015-09-22 13:21:21 +0300

    Initial commit

---
+++ app/src/androidTest/java/com/kurkov/p0811_handlersimplemessage/ApplicationTest.java
@@ -0,0 +1,13 @@
+package com.kurkov.p0811_handlersimplemessage;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
